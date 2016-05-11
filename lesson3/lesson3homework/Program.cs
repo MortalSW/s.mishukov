0e7c30a733aaa41bff27ef9e545657ca03da1520
serg <s.mishukov@2gis.ru>
2016-05-11 22:25:24 +0600

    homework (dictionary)

--- lesson3/lesson3homework/Program.cs
+++ lesson3/lesson3homework/Program.cs
@@ -10,7 +10,46 @@ namespace lesson3homework
     {
         static void Main(string[] args)
         {
+            string str = GetString();
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
 
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!charCount.ContainsKey(str[i]))
+                {
+                    for (int j = i; j < str.Length; j++)
+                    {
+                        if (str[i] == str[j])
+                        {
+                            if (charCount.ContainsKey(str[i]))
+                            {
+                                charCount[str[i]] = charCount[str[i]] + 1;
+                            }
+                            else
+                              charCount.Add(str[i], 1);
+                        }
+                    }
+                }
+            } //какой-то сложный цикл, как проще?
+
+            foreach (KeyValuePair<char,int> kv in charCount)
+            {
+                Print(kv.Key.ToString()+" "+kv.Value.ToString());
+            }
+
+            Console.ReadKey();
         }
+        private static string GetString()
+        {
+            string Val = "";
+            Console.Write("Enter STRING value: ");
+            Val = Console.ReadLine();
+            return Val;
+        }
+        private static void Print(string Val)
+        {
+            Console.WriteLine(Val);
+        }
+
     }
 }
