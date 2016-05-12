36e5c487a0a3e903a76909147f3bbb54e45bd699
MoRTaL <s.mishukov@2gis.ru>
2016-05-12 10:40:36 +0600

    new morning version

--- lesson3/lesson3homework2/Program.cs
+++ lesson3/lesson3homework2/Program.cs
@@ -12,25 +12,47 @@ namespace lesson3homework2
         {
             List<int> integers = new List<int>();
 
-            do
-            {
-                int i;
-                if ((i = GetInt()) != -1)
-                    integers.Add(i);
-                else
-                    break;
-            } while (true);
+            Print("Enter values, when -1, then exit");
 
+            { //так вложим чтобы i была локальной для цикла этого только
+                int i;
+                do
+                {
+                    if ((i = GetInt()) != -1)
+                        integers.Add(i);
+                    else
+                        break;
+                } while (true);
+            }
+
+            Print("\nVersion 1:");
             foreach (int i in integers.Distinct())
             {
-                if (integers.Where(x => x == i).Count() > 1) {
-                    Print(i);
+                if (integers.Where(item => item == i).Count() > 1)
+                {
+                    Print(i+" has duplicates");
                 }
             } // СПЛЮ уже, никак не пойму как сделать по другому... уже и с хэшсетом с отбрасыванием пробовал
-              // но никак не пойму как сделать.... и с findindex и findindexlast мутил... ошибка
+              // и с findindex и findindexlast мутил... ошибка
               // или это лучший вариант? всетаки System.Linq по дефолту в проекте )
               // можно конечно заводить несколько листов и во второй пихать если есть такой при вводе, 
               // но это неправильно, хочется научиться работать с листами не при вводе а при наличии такового
+
+
+            // во придумаааал ))
+            // утро вечера мудреней
+            Print("\nVersion 2:");
+            integers.Sort(); 
+            int last = -1;
+
+            for (int i = 0; i < integers.Count - 1; i++)
+            {
+                if (integers[i] == integers[i+1] && last != integers[i])
+                {
+                    Print("" + integers[i] + " has duplicates");
+                    last = integers[i];
+                }
+            }
 
             Console.ReadKey();
         }
