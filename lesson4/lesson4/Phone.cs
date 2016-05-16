fc3ecb401d659c5ed52c9027823100857e8580fa
serg <s.mishukov@2gis.ru>
2016-05-16 19:43:00 +0600

    lesson4

---
+++ lesson4/lesson4/Phone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Phone
+    {
+        private int cityCode = -1;
+        private int phone;
+
+        public Phone(int cityCode,int phone)
+        {
+            this.cityCode = cityCode;
+            this.phone = phone;
+        }
+        public Phone(int phone)
+        {
+            this.cityCode = -1;
+            this.phone = phone;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            if (cityCode != -1)
+            {
+                result = $"({cityCode}) {phone}";
+            }
+            else {
+                result = $"{phone}";
+            }
+            return result;
+        }
+
+    }
+}
---
+++ lesson4/lesson4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // classwork
+            Phone phone1 = new Phone(999999);
+            Phone phone2 = new Phone(3854, 369664);
+
+            Console.WriteLine(phone1.ToString());
+            Console.WriteLine(phone2.ToString());
+
+            Console.ReadKey();
+
+            //classwork 2
+            // к сожалению не понял что нужно сделать, 
+            // ибо как бы и так всё прикрыто извне от прямого вмешательства
+            // было бы неплохо услышать или увидеть пояснения
+            // ну или на доп. занятии.
+        }
+    }
+}
