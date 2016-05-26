fbfa4b15af733bc8cc39262ebd5fa57fa11aaf23
Serg <satai@ngs.ru>
2016-05-26 14:09:59 +0600

    .

--- lesson7/UnitTest/UnitTest1.cs
+++ lesson7/UnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lesson7homework;
 
 namespace UnitTest
 {
@@ -7,9 +8,23 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSum()
         {
             Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 2);
+
+            f1 = f1 + f2;
+
+            Assert.AreEqual(f1.upPart, 1);
+            Assert.AreEqual(f1.upPart, 1);
+        }
+
+        public void TestSimplify()
+        {
+            Fraction f = new Fraction(4, 4);
+
+            Assert.AreEqual(f.upPart, 1);
+            Assert.AreEqual(f.downPart, 1);
         }
     }
 }
