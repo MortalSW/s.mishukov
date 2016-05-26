0300424782d5713d98eb36a2775bee25b690cc47
Serg <satai@ngs.ru>
2016-05-26 14:40:58 +0600

    .

--- lesson7/UnitTest/UnitTest1.cs
+++ lesson7/UnitTest/UnitTest1.cs
@@ -19,12 +19,33 @@ namespace UnitTest
             Assert.AreEqual(f1.upPart, 1);
         }
 
+        [TestMethod]
         public void TestSimplify()
         {
             Fraction f = new Fraction(4, 4);
 
             Assert.AreEqual(f.upPart, 1);
             Assert.AreEqual(f.downPart, 1);
+        }
+
+        [TestMethod]
+      
+        public void TestZeroDownPart1()
+        {
+            try
+            {
+                Fraction f = new Fraction(1, 0);
+                Assert.Fail();
+            }
+            catch (DivideByZeroException ex) { }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestZeroDownPart2()
+        {
+                Fraction f = new Fraction(1, 0);
+         
         }
     }
 }
