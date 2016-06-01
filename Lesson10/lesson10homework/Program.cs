7dfe49df91c82e6b6ff04c681e8e75b86f54a9b7
Serg <s.mishukov@2gis.ru>
2016-06-01 23:56:05 +0600

    метод CanAccessFile теперь принимает метод доступа

--- Lesson10/lesson10homework/Program.cs
+++ Lesson10/lesson10homework/Program.cs
@@ -10,17 +10,17 @@ namespace lesson10homework
 {
     class Program
     {
-        private static bool CanAccessFile(string FileName)
+        private static bool CanAccessFile(string FileName, FileAccess fileAccesMethod)
         {
             try
             {
-                FileStream f = new FileInfo(FileName).Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+                FileStream f = new FileInfo(FileName).Open(FileMode.Open, fileAccesMethod, FileShare.None);
                 f.Dispose();
                 return true;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Cannot open " + FileName + " for writing");
+                Console.WriteLine("Cannot open " + FileName + " for "+fileAccesMethod);
                 Console.WriteLine("Exception: " + ex.Message);
             }
             return false;
@@ -29,23 +29,30 @@ namespace lesson10homework
         {
             //вот так мне не оч нравится. как лучше доставать директорию проекта?
             //уменьшив на 2 слэша от AppDomain.CurrentDomain.BaseDirectory - не вариант, то-же самое получается
-            string text = File.ReadAllText(@"..\..\flowCards.Card.xml"); 
-            XElement[] contactValues = XElement.Parse(text).Element("Contacts").Elements().ToArray();
-
-            if (CanAccessFile(@"..\..\promoContacts") && CanAccessFile(@"..\..\usualContacts"))
+            if (CanAccessFile(@"..\..\flowCards.Card.xml", FileAccess.Read))
             {
-                using (StreamWriter promoContactsFile = new StreamWriter(@"..\..\promo:Contacts"))
-                using (StreamWriter usualContactsFile = new StreamWriter(@"..\..\usualContacts"))
+                string text = File.ReadAllText(@"..\..\flowCards.Card.xml");
+                XElement[] contactValues = XElement.Parse(text).Element("Contacts").Elements().ToArray();
+                
+                if (CanAccessFile(@"..\..\p:romoContacts", FileAccess.Write) && 
+                    CanAccessFile(@"..\..\usualContacts", FileAccess.Write))
                 {
-                    foreach (XElement contactValue in contactValues)
+                
+                    using (StreamWriter promoContactsFile = new StreamWriter(@"..\..\promoContacts"))
+                    using (StreamWriter usualContactsFile = new StreamWriter(@"..\..\usualContacts"))
                     {
-                        if (contactValue.Attribute("IsPromotional").Value == "true")
+                        foreach (XElement contactValue in contactValues)
                         {
-                            promoContactsFile.WriteLine(contactValue.Attribute("Value").Value + " [" + (contactValue.Attribute("Description") == null ? "" : contactValue.Attribute("Description").Value.ToString()) + "]");
-                        }
-                        else
-                        {
-                            usualContactsFile.WriteLine(contactValue.Attribute("Value").Value + " [" + (contactValue.Attribute("Description") == null ? "" : contactValue.Attribute("Description").Value.ToString()) + "]");
+                            if (contactValue.Attribute("IsPromotional").Value == "true")
+                            {
+                                promoContactsFile.WriteLine(contactValue.Attribute("Value").Value + " [" + 
+                                    (contactValue.Attribute("Description") == null ? "" : contactValue.Attribute("Description").Value.ToString()) + "]");
+                            }
+                            else
+                            {
+                                usualContactsFile.WriteLine(contactValue.Attribute("Value").Value + " [" + 
+                                    (contactValue.Attribute("Description") == null ? "" : contactValue.Attribute("Description").Value.ToString()) + "]");
+                            }
                         }
                     }
                 }
